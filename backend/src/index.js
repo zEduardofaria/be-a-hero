a87b1fc69ed8c1fdae12d0606354ff1f5ed89b86
Eduardo Faria <z.eduardofaria@gmail.com>
2020-03-31 22:58:56 -0300

    Mongo atlas connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://root:root@cluster0-vo74s.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json());
 
